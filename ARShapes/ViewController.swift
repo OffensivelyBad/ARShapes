063666ee67937de526b10ad6eff50d968b7845c9
SRoller <sroller@justfab.com>
2018-05-09 23:33:51 -0400

    Creates shape in code

--- ARShapes/ViewController.swift
+++ ARShapes/ViewController.swift
@@ -13,13 +13,26 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private let useScene = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
         sceneView.showsStatistics = true
-        let scene = SCNScene(named: "art.scnassets/TestScene.scn")!
+        var scene = SCNScene()
+        if useScene {
+            scene = SCNScene(named: "art.scnassets/TestScene.scn")!
+        }
         sceneView.scene = scene
+        createShapes()
+    }
+    
+    func createShapes() {
+        let pyramid = SCNPyramid(width: 0.2, height: 0.2, length: 0.2)
+        pyramid.firstMaterial?.diffuse.contents = UIColor.blue
+        let pyramidNode = SCNNode(geometry: pyramid)
+        pyramidNode.position.z = -0.8
+        sceneView.scene.rootNode.addChildNode(pyramidNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
